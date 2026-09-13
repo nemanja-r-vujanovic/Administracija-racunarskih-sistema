aab288d5e7574a725d5f7fe2b60f36159349aea8
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-13 17:57:11 +0200

    Update 2_komentari_ispis-echo-printf.sh

--- 3_Bash-skripte/2_komentari_ispis-echo-printf.sh
+++ 3_Bash-skripte/2_komentari_ispis-echo-printf.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 # ----------------------------------------------------------------------------------------------------
-# Bash                   # Python
+# Bash                       # Python
 
 # ----------------------------------------------------------------------------------------------------
-# Jednolinijski komentar # # Jednolinijski komentar
+# Jednolinijski komentar     # # Jednolinijski komentar
 
 # ----------------------------------------------------------------------------------------------------
 # echo
 
-echo "Zdravo svete"      # print("Zdravo svete")
+echo "Zdravo svete"          # print("Zdravo svete")
 
 # ----------------------------------------------------------------------------------------------------
 # printf
 
-printf "Zdravo svete\n"  # print("Zdravo svete")
+printf "Zdravo svete\n"      # print("Zdravo svete")
+
+# ----------------------------------------------------------------------------------------------------
+# Ostali ispisi
+
+echo {fajl1,fajl2,fajl3}     # Ispis: fajl1 fajl2 fajl3
+echo {fajl1,fajl2,fajl3}.txt # Ispis: fajl1.txt fajl2.txt fajl3.txt
+echo {0..10}                 # Ispis: od 0 do 10
+echo {0..10..2}              # Ispis: od 0 do 10 po 2
+echo {10..0}                 # Ispis: od 10 do 0
+echo {10..0..2}              # Ispis: od 10 do 0 po 2
+echo {A..Z}                  # Ispis: od A do Z
+echo {A..Z..2}               # Ispis: od A do Z po 2
+echo {Z..A}                  # Ispis: od Z do A
+echo {Z..A..2}               # Ispis: od Z do A po 2
 
 # ----------------------------------------------------------------------------------------------------
