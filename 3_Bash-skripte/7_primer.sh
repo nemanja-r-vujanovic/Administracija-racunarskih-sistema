b6bffbcfb7ee528a71b4bbeb6eb7792aa2be525d
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-13 09:56:34 +0200

    Update 7_primer.sh

--- 3_Bash-skripte/7_primer.sh
+++ 3_Bash-skripte/7_primer.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
 # ----------------------------------------------------------------------------------------------------
-# Bash                           # Python
+# Bash                              # Python
 
 # ----------------------------------------------------------------------------------------------------
 # Unosi
 
-echo -n "Unesite A: "            # print("Unesite A: ", end="")
-read a                           # a = input()
-echo -n "Unesite B: "            # print("Unesite B: ", end="")
-read b                           # b = input()
+echo -n "Unesite A: "               # print("Unesite A: ", end="")
+read a                              # a = input()
+echo -n "Unesite B: "               # print("Unesite B: ", end="")
+read b                              # b = input()
 
 # ----------------------------------------------------------------------------------------------------
 # Proizvod
 
-proizvod=$(( "$a" * "$b" ))      # proizvod = a * b
+proizvod=$(( "$a" * "$b" ))         # proizvod = a * b
 
 # ----------------------------------------------------------------------------------------------------
+# for
 
-for (( i = 0; i < 10; i++ )); do # for i in range(0, 10, 1):
-	echo "$proizvod"             #     print(f"{proizvod}")
+for (( i = 0; i < 10; i++ )); do    # for i in range(0, 10, 1):
+	echo "$proizvod"                #     print(f"{proizvod}")
+done
+
+# ----------------------------------------------------------------------------------------------------
+# Ugnjezdena petlja
+
+for (( i = 0; i < 5; i++ )); do     # for i in range(0, 5, 1):
+	for (( j = 0; j < 5; j++ )); do #     for j in range(0, 5, 1):
+		printf "0 "                 #         print("0 ", end="")
+	done
+		printf "\n"                 #     print()
 done
 
 # ----------------------------------------------------------------------------------------------------
