161c9e673738b9b0e6c96b4d56e16e4c04617c73
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-15 21:57:27 +0200

    Update 7_stringovi-transformacije-isecanje_nizovi_recnici_sel-promenljive_specificni-operatori.sh

--- 3_Bash-skripte/7_stringovi-transformacije-isecanje_nizovi_recnici_sel-promenljive_specificni-operatori.sh
+++ 3_Bash-skripte/7_stringovi-transformacije-isecanje_nizovi_recnici_sel-promenljive_specificni-operatori.sh
@@ -85,6 +85,12 @@ declare -a brojevi=(1 2 3)
 brojevi+=(4 5)
 echo "${brojevi[@]}"                            # 1 2 3 4 5
 
+vrednosti=("abc" "def" "ghi" "jkl" "mno")
+echo "${vrednosti[-1]}"                         # mno
+echo "${#vrednosti[1]}"                         # 3
+echo "${vrednosti[@]:3:2}"                      # jkl mno
+echo "${!vrednosti[@]}"                         # 0 1 2 3 4
+
 # ----------------------------------------------------------------------------------------------------
 # Recnik
 
