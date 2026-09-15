95cb40749f800a3fe0ef70682897a2348dcb93be
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-15 13:56:38 +0200

    Update 7_stringovi-transformacije-isecanje_nizovi_recnici_sel-promenljive_specificni-operatori.sh

--- 3_Bash-skripte/7_stringovi-transformacije-isecanje_nizovi_recnici_sel-promenljive_specificni-operatori.sh
+++ 3_Bash-skripte/7_stringovi-transformacije-isecanje_nizovi_recnici_sel-promenljive_specificni-operatori.sh
@@ -125,29 +125,30 @@ echo "PID poslednjeg procesa: $!"
 echo "Izlazni status poslednje komande: $?"
 
 # ----------------------------------------------------------------------------------------------------
+
 # Operatori za proveru fajlova:
-# -e "$fajl"            # da li postoji fajl/direktorijum
-# -s "$fajl"            # da li postoji fajl/direktorijum i ima veličinu > 0 bajtova
-# -f "$fajl"            # da li je fajl
-# -d "$fajl"            # da li je direktorijum
-# -L "$fajl"            # da li je simbolički link
-# -b "$fajl"            # da li je block device
-# -c "$fajl"            # da li je character device
-# -p "$fajl"            # da li je FIFO
-# -S "$fajl"            # da li je socket
+# -e "$fajl"                                    # da li postoji fajl/direktorijum
+# -s "$fajl"                                    # da li postoji fajl/direktorijum i ima velicinu > 0 bajtova
+# -f "$fajl"                                    # da li je fajl
+# -d "$fajl"                                    # da li je direktorijum
+# -L "$fajl"                                    # da li je simbolicki link
+# -b "$fajl"                                    # da li je block device
+# -c "$fajl"                                    # da li je character device
+# -p "$fajl"                                    # da li je FIFO
+# -S "$fajl"                                    # da li je socket
 
 # Operatori za poređenje fajlova:
-# "$fajl1" -nt "$fajl2" # da li je fajl1 noviji od fajla2
-# "$fajl1" -ot "$fajl2" # da li je fajl1 stariji od fajla2
-# "$fajl1" -ef "$fajl2" # da li fajl1 i fajl2 predstavljaju isti fajl
+# "$fajl1" -nt "$fajl2"                         # da li je fajl1 noviji od fajla2
+# "$fajl1" -ot "$fajl2"                         # da li je fajl1 stariji od fajla2
+# "$fajl1" -ef "$fajl2"                         # da li fajl1 i fajl2 predstavljaju isti fajl
 
 # Operatori dozvola i posebnih bitova:
-# -r "$fajl"            # da li je čitljiv
-# -w "$fajl"            # da li je upisiv
-# -x "$fajl"            # da li je izvršiv
-# -u "$fajl"            # da li ima setuid bit
-# -g "$fajl"            # da li ima setgid bit
-# -k "$fajl"            # da li ima sticky bit
+# -r "$fajl"                                    # da li je citljiv
+# -w "$fajl"                                    # da li je upisiv
+# -x "$fajl"                                    # da li je izvrsiv
+# -u "$fajl"                                    # da li ima setuid bit
+# -g "$fajl"                                    # da li ima setgid bit
+# -k "$fajl"                                    # da li ima sticky bit
 
 # Da li postoji komanda:
 naziv_komande="git"
