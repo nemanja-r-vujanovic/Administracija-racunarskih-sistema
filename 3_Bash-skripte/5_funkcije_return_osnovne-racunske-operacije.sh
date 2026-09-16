e37c9db04f4497c107741ab1fafae9ee690a42f3
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-16 19:17:36 +0200

    Fix increment syntax in while loop

--- 3_Bash-skripte/5_funkcije_return_osnovne-racunske-operacije.sh
+++ 3_Bash-skripte/5_funkcije_return_osnovne-racunske-operacije.sh
@@ -67,7 +67,7 @@ function ulepsaj_ispis()                      # def ulepsaj_ispis():
 	string=""                                 #     string = ""
 	while (( 100 > "$i" )); do                #     while (100 > i):
 		string+="-"                           #         string += "-"
-		(( i++ ))
+		(( i++ ))                             #         i += 1
 	done
 	echo "$string"                            #     return string
 }
