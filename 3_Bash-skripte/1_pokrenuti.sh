35051c72450b20976bcdfda20662aaff69799499
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-16 19:35:04 +0200

    Add files via upload

--- 3_Bash-skripte/1_pokrenuti.sh
+++ 3_Bash-skripte/1_pokrenuti.sh
@@ -4,9 +4,9 @@ chmod +x *".sh"
 for (( i = 2; i <= 8; i++ )); do
 	echo "Primer broj $i:"
 	if (( i != 8 )); then
-		./"$i_*.sh"
+		./"$i"*
 	else
-		./"$i_*.sh" "tekst1" "tekst2"
+		./"$i"* "tekst1" "tekst2"
 	fi
 	echo
 done
