1f14f3c74315937f7b4b5a20b487d5ad9747301c
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-17 22:33:10 +0200

    Update 2_komentari_ispis-echo-printf.sh

--- 3_Bash-skripte/2_komentari_ispis-echo-printf.sh
+++ 3_Bash-skripte/2_komentari_ispis-echo-printf.sh
@@ -30,4 +30,45 @@ echo {A..Z..2}               # od A do Z po 2
 echo {Z..A}                  # od Z do A
 echo {Z..A..2}               # od Z do A po 2
 
+# Ispis u bojama:
+
+echo -e "\e[41m          \e[0m"
+echo -e "\e[44m          \e[0m"
+echo -e "\e[47m          \e[0m"
+
+# ANSI kodna tabela
+
+# 1. Boje teksta:
+# Crna \e[30m
+# Crvena \e[31m
+# Zelena \e[32m
+# Zuta \e[33m
+# Plava \e[34m
+# Roza \e[35m
+# Svetloplava \e[36m
+# Bela \e[37m
+
+# 2. Boje pozadine:
+# Crna \e[40m
+# Crvena \e[41m
+# Zelena \e[42m
+# Zuta \e[43m
+# Plava \e[44m
+# Roza \e[45m
+# Svetloplava \e[46m
+# Bela \e[47m
+
+# 3. Stilovi teksta:
+# Reset: \e[0m
+# Bold: \e[1m
+# Italic: \e[3m
+# Underline: \e[4m
+# Blink: \e[5m
+
+# Kombinovanje sa bojama odvija se razdvajanjem sa tackom zarezom.
+
+# 4. Primer kombinovanja:
+# Podebljan tekst sa crvenom pozadinom i belim slovima
+# echo -e "\e[1;41;37m Upozorenje! \e[0m"
+
 # ----------------------------------------------------------------------------------------------------
