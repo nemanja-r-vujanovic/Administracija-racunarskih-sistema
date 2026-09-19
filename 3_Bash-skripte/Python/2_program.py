6b362769d05e1bf4accbfff09e838e20eb995471
Nemanja Vujanović <251783639+nemanja-r-vujanovic@users.noreply.github.com>
2026-09-19 13:03:04 +0200

    Add files via upload

--- 3_Bash-skripte/Python/2_program.py
+++ 3_Bash-skripte/Python/2_program.py
@@ -1,8 +1,8 @@
-import sys
+import subprocess
 
-if (len(sys.argv) != 2):
-	print("Prosledite tacno jedan argument!")
-else:
-	arg = int(sys.argv[1])
-	for i in range(arg, -1, -1):
-		print(i)
+def pokreni_komandu(komanda):
+	rezultat = subprocess.run(komanda, shell=True, capture_output=True, text=True)
+	return rezultat.stdout
+
+rezultat = pokreni_komandu("ls -l")
+print(rezultat)
--- 3_Bash-skripte/Python/3_program.py
+++ 3_Bash-skripte/Python/3_program.py
@@ -1,8 +1,8 @@
-import subprocess
+import sys
 
-def pokreni_komandu(komanda):
-	rezultat = subprocess.run(komanda, shell=True, capture_output=True, text=True)
-	return rezultat.stdout
-
-rezultat = pokreni_komandu("ls -l")
-print(rezultat)
+if (len(sys.argv) != 2):
+	print("Prosledite tacno jedan argument!")
+else:
+	arg = int(sys.argv[1])
+	for i in range(arg, -1, -1):
+		print(i)
